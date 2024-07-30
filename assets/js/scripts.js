6b3e9723e8800ec4d171f0cdd019c74da2921a07
Lloyd26 <ademic26@yahoo.com>
2024-07-30 23:14:23 +0800

    Add functions for translating Densely Packed BCD to Decimal

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -61,7 +61,7 @@ function validateInput(type, value, error_container) {
     return true;
 }
 
-function convertGenerator(e) {
+function convertGenerator() {
     let input = document.querySelector("#input-generator").value;
 
     let error_container = document.querySelector("#error-generator");
@@ -80,13 +80,23 @@ function convertGenerator(e) {
     }
 
 
+    if (document.querySelector("#mode-densely-packed").checked) {
+        if (input.length < 3) input = String(input).padStart(3, '0');
+        let packed = bcdUnpackedPacked("packed", input).join("");
+        console.log(packed);
+        setOutput(output, bcdDenselyPacked(packed).join(""));
+    }
 }
 
-function convertTranslator(e) {
+function convertTranslator() {
     let input = document.querySelector("#input-translator").value;
 
     let error_container = document.querySelector("#error-translator");
     validateInput("translator", input, error_container);
+
+    let output = document.querySelector("#output-translator");
+
+    setOutput(output, packedToDecimal(bcdDecimal(input)));
 }
 
 function bcdUnpackedPacked(mode, decimal) {
@@ -111,6 +121,224 @@ function bcdUnpackedPacked(mode, decimal) {
         result.map((v, i) => result[i] = "0000" + v);
     }
 
+    return result;
+}
+
+function bcdDenselyPacked(decimal) {
+    const a = String(decimal).charAt(0);
+    const b = String(decimal).charAt(1);
+    const c = String(decimal).charAt(2);
+    const d = String(decimal).charAt(3);
+    const e = String(decimal).charAt(4);
+    const f = String(decimal).charAt(5);
+    const g = String(decimal).charAt(6);
+    const h = String(decimal).charAt(7);
+    const i = String(decimal).charAt(8);
+    const j = String(decimal).charAt(9);
+    const k = String(decimal).charAt(10);
+    const m = String(decimal).charAt(11);
+
+    let r = d;
+    let u = h;
+    let y = m;
+
+    let p = "",
+        q = "",
+        s = "",
+        t = "",
+        w = "",
+        x = "";
+
+    let v = "1"
+
+    if (a === "0") {
+        p = b;
+        q = c;
+
+        if (e === "0") {
+            s = f;
+            t = g;
+
+            if (i === "0") {
+                w = j;
+                x = k;
+                v = "0";
+            }
+            if (i === "1") {
+                w = "0";
+                x = "0";
+            }
+        }
+
+        if (e === "1") {
+            w = i;
+            x = "1";
+
+            if (i === "0") {
+                s = j;
+                t = k;
+            }
+            if (i === "1") {
+                s = "1";
+                t = "0";
+            }
+        }
+    }
+
+    if (a === "1") {
+        w = "1";
+
+        if (e === "0") {
+            x = i;
+
+            if (i === "0") {
+                p = j;
+                q = k;
+                s = f;
+                t = g;
+            }
+            if (i === "1") {
+                p = f;
+                q = g;
+                s = "0";
+                t = "1";
+            }
+        }
+
+        if (e === "1") {
+            s = i;
+            t = i;
+            x = "1";
+
+            if (i === "0") {
+                p = j;
+                q = k;
+            }
+            if (i === "1") {
+                p = "0";
+                q = "0";
+            }
+        }
+    }
+
+    let result = [p, q, r, s, t, u, v, w, x, y];
+    return result;
+}
+
+function bcdDecimal(bcd) {
+    const p = String(bcd).charAt(0);
+    const q = String(bcd).charAt(1);
+    const r = String(bcd).charAt(2);
+    const s = String(bcd).charAt(3);
+    const t = String(bcd).charAt(4);
+    const u = String(bcd).charAt(5);
+    const v = String(bcd).charAt(6);
+    const w = String(bcd).charAt(7);
+    const x = String(bcd).charAt(8);
+    const y = String(bcd).charAt(9);
+
+    let d = r;
+    let h = u;
+    let m = y;
+
+    let a = "0",
+        b = "0",
+        c = "0",
+        e = "0",
+        f = "0",
+        g = "0",
+        i = "0",
+        j = "0",
+        k = "0";
+
+    if (v === "0") {
+        a = "0";
+        e = "0";
+        i = "0";
+
+        b = p;
+        c = q;
+        f = s;
+        g = t;
+        j = w;
+        k = x;
+    }
+    if (v === "1") {
+        if (w === "0") {
+            a = "0";
+
+            b = p;
+            c = q;
+
+            if (x === "0") {
+                e = "0";
+                i = "1";
+
+                f = s;
+                g = t;
+            }
+            if (x === "1") {
+                e = "1";
+                i = "0";
+
+                j = s;
+                k = t;
+            }
+        }
+        if (w === "1") {
+            if (x === "0") {
+                a = "1";
+                e = "0";
+                i = "0";
+
+                j = p;
+                k = q;
+                f = s;
+                g = t;
+            }
+            if (x === "1") {
+                if (s === "0") {
+                    a = "1";
+
+                    if (t === "0") {
+                        e = "1";
+                        i = "0";
+
+                        j = p;
+                        k = q;
+                    }
+                    if (t === "1") {
+                        e = "0";
+                        i = "1";
+
+                        f = p;
+                        g = q;
+                    }
+                }
+                if (s === "1") {
+                    a = t;
+                    e = "1";
+                    i = "1";
+
+                    if (t === "0") {
+                        b = p;
+                        c = q;
+                    }
+                }
+            }
+        }
+    }
+
+    let result = [a, b, c, d, e, f, g, h, i, j, k, m];
+    return result;
+}
+
+function packedToDecimal(bcd_decimal) {
+    let digit0 = [bcd_decimal[0], bcd_decimal[1], bcd_decimal[2], bcd_decimal[3]];
+    let digit1 = [bcd_decimal[4], bcd_decimal[5], bcd_decimal[6], bcd_decimal[7]];
+    let digit2 = [bcd_decimal[8], bcd_decimal[9], bcd_decimal[10], bcd_decimal[11]];
+
+    let result = String(parseInt(digit0.join(""), 2)) + String(parseInt(digit1.join(""), 2)) + String(parseInt(digit2.join(""), 2));
     return result;
 }
 
