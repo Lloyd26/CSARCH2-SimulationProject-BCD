8b6b60d8a010604673400c8e1655a5d68407fdbb
Lloyd26 <ademic26@yahoo.com>
2024-07-30 15:07:50 +0800

    Add form reset feature

---
+++ scripts.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll(".btn-reset").forEach(r => r.addEventListener("click", resetForm));
+})
+
+function resetForm(e) {
+    let tab_content = e.currentTarget.closest(".tab-content");
+    let input_generator = tab_content.querySelector("#input-generator");
+    let input_translator = tab_content.querySelector("#input-translator");
+
+    if (input_generator !== undefined) input_generator.value = "";
+    if (input_translator !== undefined) input_translator.value = "";
+
+    if (tab_content.querySelector("#bcd-generator") !== undefined) {
+        tab_content.querySelectorAll("input[type=radio][name=mode-generator]").forEach(bcd_mode_radio => bcd_mode_radio.checked = false);
+
+    }
+}
